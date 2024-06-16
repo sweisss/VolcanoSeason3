beafe0323a24b98132f12a6e46942dab5e4d3824
Seth Weiss <sweisss@gmail.com>
2024-06-15 21:13:12 -0700

    Add forecast links and link names to strings resource

--- app/src/main/java/com/example/volcanoseason3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/volcanoseason3/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.volcanoseason3.R
 import com.example.volcanoseason3.data.gallery.MountainLink
 import com.example.volcanoseason3.databinding.FragmentHomeBinding
 
@@ -24,58 +25,61 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+//        Log.d("HomeFragment", "LOGGED")
+//        Log.d("HomeFragment", getString(R.string.mt_adams_link))
+
         val mountainLinks = arrayListOf(
             MountainLink(
-                "\uD83C\uDF0B Mt Adams",
-                "https://www.mountain-forecast.com/peaks/Mount-Adams/forecasts/3741"
+                getString(R.string.link_name_adams),
+                getString(R.string.link_adams)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mount Bachelor",
-                "https://www.mountain-forecast.com/peaks/Mount-Bachelor/forecasts/2764"
+                getString(R.string.link_name_bachelor),
+                getString(R.string.link_bachelor)
             ),
             MountainLink(
-                "\uD83C\uDF0B Broken Top",
-                "https://www.mountain-forecast.com/peaks/Broken-Top/forecasts/2797"
+                getString(R.string.link_name_broken_top),
+                getString(R.string.link_broken_top)
             ),
             MountainLink(
-                "\uD83C\uDF0B Diamond Peak",
-                "https://www.mountain-forecast.com/peaks/Diamond-Peak-Oregon/forecasts/2665"
+                getString(R.string.link_name_diamond),
+                getString(R.string.link_diamond)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mt Hood",
-                "https://www.mountain-forecast.com/peaks/Mount-Hood/forecasts/3426"
+                getString(R.string.link_name_hood),
+                getString(R.string.link_hood)
             ),
             MountainLink(
-                "\uD83C\uDF0B Lassen Peak",
-                "https://www.mountain-forecast.com/peaks/Lassen-Peak/forecasts/3187"
+                getString(R.string.link_name_lassen),
+                getString(R.string.link_lassen)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mount McLoughlin",
-                "https://www.mountain-forecast.com/peaks/Mount-McLoughlin/forecasts/2894"
+                getString(R.string.link_name_mcloughlin),
+                getString(R.string.link_mcloughlin)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mt Shasta",
-                "https://www.mountain-forecast.com/peaks/Mount-Shasta/forecasts/4317"
+                getString(R.string.link_name_shasta),
+                getString(R.string.link_shasta)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mt St Helens",
-                "https://www.mountain-forecast.com/peaks/Mount-Saint-Helens/forecasts/2549"
+                getString(R.string.link_name_helens),
+                getString(R.string.link_helens)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mt Thielsen",
-                "https://www.mountain-forecast.com/peaks/Mount-Thielsen/forecasts/2797"
+                getString(R.string.link_name_thielsen),
+                getString(R.string.link_thielsen)
             ),
             MountainLink(
-                "\uD83C\uDF0B Three Sisters",
-                "https://www.mountain-forecast.com/peaks/Three-Sisters/forecasts/3157"
+                getString(R.string.link_name_sisters),
+                getString(R.string.link_sisters)
             ),
             MountainLink(
-                "\uD83C\uDF0B Mt Washington",
-                "https://www.mountain-forecast.com/peaks/Mount-Washington-3/forecasts/2376"
+                getString(R.string.link_name_washington),
+                getString(R.string.link_washington)
             ),
             MountainLink(
-                "\uD83C\uDF24️  Bend NOAA",
-                "https://forecast.weather.gov/MapClick.php?lat=44.06&lon=-121.3#.YsZ5b-zMLeo"
+                getString(R.string.link_name_bend_noaa),
+                getString(R.string.link_bend_noaa)
             )
         )
 
