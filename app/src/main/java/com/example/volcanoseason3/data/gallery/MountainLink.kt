cb114f017aae4dfd5e0330fb108063d9aeda2f12
Seth Weiss <sweisss@gmail.com>
2024-06-21 21:21:22 -0700

    Add room annotations to MountainLink data class

--- app/src/main/java/com/example/volcanoseason3/data/gallery/MountainLink.kt
+++ app/src/main/java/com/example/volcanoseason3/data/gallery/MountainLink.kt
@@ -1,6 +1,10 @@
 package com.example.volcanoseason3.data.gallery
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class MountainLink(
-    val name: String,
+    @PrimaryKey val name: String,
     val link: String
 )
