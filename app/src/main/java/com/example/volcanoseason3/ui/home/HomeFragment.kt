25b0bc59cb9252553b1a07e3a2198cc265c0ff71
Seth Weiss <sweisss@gmail.com>
2024-06-26 00:17:38 -0700

    Remove old commented out code

--- app/src/main/java/com/example/volcanoseason3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/volcanoseason3/ui/home/HomeFragment.kt
@@ -50,8 +50,6 @@ class HomeFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
         Log.d("HomeFragment", "Called onCreateOptionsMenu; $menu")
         inflater.inflate(R.menu.menu_home, menu)
-//        menu.findItem(R.id.action_settings)?.isVisible = true
-//        menu.findItem(R.id.action_settings2)?.isVisible = true
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
