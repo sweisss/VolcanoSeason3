10944f351a987aa08b011126c8fe6eb7f91ba0f2
Seth Weiss <sweisss@gmail.com>
2024-10-08 20:13:21 -0700

    Update ChecklistFragment with RecyclerView and to be implemented ChecklistAdapter

--- app/src/main/java/com/example/volcanoseason3/ui/checklist/ChecklistFragment.kt
+++ app/src/main/java/com/example/volcanoseason3/ui/checklist/ChecklistFragment.kt
@@ -10,16 +10,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.volcanoseason3.R
 import com.example.volcanoseason3.databinding.FragmentChecklistBinding
 
 class ChecklistFragment : Fragment() {
     private var _binding: FragmentChecklistBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var checklistViewModel: ChecklistViewModel
+    private lateinit var adapter: ChecklistAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,16 +38,22 @@ class ChecklistFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        _binding = FragmentChecklistBinding.inflate(inflater, container, false)
+
         val checklistViewModel =
             ViewModelProvider(this).get(ChecklistViewModel::class.java)
 
-        _binding = FragmentChecklistBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // Set up the RecyclerView and Adapter
+        val recyclerView = binding.recyclerViewChecklist
+        adapter = ChecklistAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val textView: TextView = binding.textChecklist
-        checklistViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // Observe Checklist Items
+        checklistViewModel.checklistItems.observe(viewLifecycleOwner, Observer { items ->
+            adapter.submitList(items)
+        })
+
+        val root: View = binding.root
         return root
     }
 
